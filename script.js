ef32b48eb6393b2e62c847600891a5e6b2743d77
HuangLinBao <tariq.92@live.com>
2021-04-30 18:39:24 +0400

    new  pages

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
     scrollLink.click(function(e) {
         e.preventDefault();
         $('body,html').animate({
-            scrollTop: $(this.hash).offset().top
+            scrollTop: $(this.hash).offset().top - 120
         }, 1000);
     });
 
@@ -16,7 +16,7 @@ $(document).ready(function() {
 
             scrollLink.each(function() {
 
-                var sectionOffset = $(this.hash).offset().top - 140;
+                var sectionOffset = $(this.hash).offset().top - 120;
 
                 if (sectionOffset <= scrollbarLocation) {
                     $(this).parent().addClass('active');
@@ -27,11 +27,88 @@ $(document).ready(function() {
         })
         //smaller header on scroll
     $(window).scroll(function() {
-        if ($(window).scrollTop()) {
-            $('header').addClass('smallHeader');
-        } else {
-            $('header').removeClass('smallHeader');
-        }
-    })
+            if ($(window).scrollTop()) {
+                $('header').addClass('smallHeader');
+            } else {
+                $('header').removeClass('smallHeader');
+            }
+        })
+        //text slider for about page
 
-})
+    const SlidePage = document.querySelector(".slidePage");
+    const forwardBtn = document.querySelector(".moveForward");
+    const backBtn = document.querySelector(".goBack");
+    const bullet = document.querySelector(".circle");
+    backBtn.disabled = true;
+    var margin = 0;
+    forwardBtn.addEventListener('click', function() {
+
+        switch (margin) {
+            case 0:
+                SlidePage.style.marginLeft = "-25%";
+                margin -= 25;
+                $(".circle.two").parent().addClass('current');
+                $(".circle.two").parent().siblings().removeClass('current');
+                break;
+
+            case -25:
+                SlidePage.style.marginLeft = "-50%";
+                margin -= 25;
+                $(".circle.three").parent().addClass('current');
+                $(".circle.three").parent().siblings().removeClass('current');
+                break;
+
+
+        }
+    });
+
+    backBtn.addEventListener('click', function() {
+        switch (margin) {
+            case -50:
+                SlidePage.style.marginLeft = "-25%";
+                margin += 25;
+                $(".circle.two").parent().addClass('current');
+                $(".circle.two").parent().siblings().removeClass('current');
+
+                break;
+
+            case -25:
+                SlidePage.style.marginLeft = "0%";
+                margin += 25;
+                $(".circle.one").parent().addClass('current');
+                $(".circle.one").parent().siblings().removeClass('current');
+                break;
+        }
+
+    });
+
+    // slider for services page
+    const scrollPage = document.querySelector(".scrollPage");
+    const desBtn = document.querySelector(".design");
+    const supBtn = document.querySelector(".supply");
+    const impBtn = document.querySelector(".implement");
+    const followBtn = document.querySelector(".follow");
+
+    desBtn.addEventListener('click', function() {
+        scrollPage.style.marginTop = "0%";
+        $(this).addClass('selected');
+        $(this).siblings().removeClass('selected');
+    });
+    supBtn.addEventListener('click', function() {
+        scrollPage.style.marginTop = "-60%";
+        $(this).addClass('selected');
+        $(this).siblings().removeClass('selected');
+    });
+    impBtn.addEventListener('click', function() {
+        scrollPage.style.marginTop = "-116%";
+        $(this).addClass('selected');
+        $(this).siblings().removeClass('selected');
+    });
+    followBtn.addEventListener('click', function() {
+        scrollPage.style.marginTop = "-180%";
+        $(this).addClass('selected');
+        $(this).siblings().removeClass('selected');
+    });
+
+
+});
